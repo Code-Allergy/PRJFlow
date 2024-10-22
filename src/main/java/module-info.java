d074e7cb39e4cae89feb94433a824d85d62b45e9
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2024-10-22 08:50:16 +0000

    added dependencies for global terms && config file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module com.cmpt370T7.PRJFlow {
 
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
+    requires org.slf4j;
+    requires java.sql;
+    requires toml4j;
 
     opens com.cmpt370T7.PRJFlow to javafx.fxml;
     exports com.cmpt370T7.PRJFlow;
