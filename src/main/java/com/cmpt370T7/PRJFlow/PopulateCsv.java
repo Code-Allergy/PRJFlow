95829498867cab469554418825feaec711d7b9b4
Zach Sousa <zss473@usask.ca>
2024-10-22 11:32:26 -0600

    appendDataToCsv func can set values and write to csv now

--- src/main/java/com/cmpt370T7/PRJFlow/PopulateCsv.java
+++ src/main/java/com/cmpt370T7/PRJFlow/PopulateCsv.java
@@ -28,5 +28,29 @@ public class PopulateCsv {
                 e.printStackTrace();
             }
         }
+
+        // Ensures the CSV data structure is large enough for y
+        // While it isn't appends more lists
+        while (csvData.size() <= y) {
+            csvData.add(new ArrayList<>());
+        }
+        // Ensures the CSV data structure is large enough for x
+        // Adds empty placeholders since csv files require ,'s for index value
+        List<String> row = csvData.get(y);
+        while (row.size() <= x) {
+            row.add("");
+        }
+
+        // Sets the value at the specific x, y position
+        row.set(x, data);
+
+        // Writes and saves data back into the CSV file
+        try (PrintWriter pw = new PrintWriter(new FileWriter(file))) {
+            for (List<String> csvRow : csvData) {
+                pw.println(String.join(",", csvRow));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
