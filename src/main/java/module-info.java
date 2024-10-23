d41ec2f84b421a579ec1d02786f97addd74450b3
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2024-10-23 06:32:00 +0000

    updated maven dependencies to add tesseract, pdfbox and jsoup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,32 @@
 module com.cmpt370T7.PRJFlow {
+    // JavaFX
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Optional JavaFX
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
+
+    // Logging
     requires org.slf4j;
+
+    // PDF parsing & OCR
+    requires org.apache.pdfbox;
+    requires org.bytedeco.tesseract;
+
+    // HTML parsing (used for Tesseract OCR output parsing)
+    requires org.jsoup;
+
+    // Global store
     requires java.sql;
+
+    // Config
     requires toml4j;
 
+    // Required for imageIO functions
+    requires java.desktop;
+
     opens com.cmpt370T7.PRJFlow to javafx.fxml;
+    exports com.cmpt370T7.PRJFlow.util;
     exports com.cmpt370T7.PRJFlow;
 }
