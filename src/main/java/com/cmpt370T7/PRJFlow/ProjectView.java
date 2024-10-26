4771a9d2f6bcc34326d057604f8c179f9f401127
wwj033 <wwj033@usask.ca>
2024-10-26 12:14:03 -0600

    Project GUI page now has 3 main columns and icons

--- src/main/java/com/cmpt370T7/PRJFlow/ProjectView.java
+++ src/main/java/com/cmpt370T7/PRJFlow/ProjectView.java
@@ -1,5 +1,6 @@
 package com.cmpt370T7.PRJFlow;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
@@ -8,16 +9,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
+import org.kordamp.ikonli.javafx.FontIcon;
+
 import java.io.FileInputStream;
 import java.net.URL;
 
 
 public class ProjectView extends VBox {
-
-    //Canvas canvas;
-
     String[] files = new String[]{"specs.pdf", "WorkPlan.pdf", "Floorplan.pdf", "ShadeCount.xlsx", "Sched.png"};
-
 
     public ProjectView() {
         this.setStyle("-fx-background-color: #eeeee4");
@@ -29,33 +28,35 @@ public class ProjectView extends VBox {
         menuBar.setMinHeight(10);
         VBox.setVgrow(menuBar, Priority.ALWAYS);
 
-
-
         GridPane body = new GridPane();
+        body.setPadding(new Insets(10, 5, 10, 5));
         body.setMinHeight(20);
         VBox.setVgrow(body, Priority.SOMETIMES);
         body.setAlignment(Pos.CENTER);
-
         body.setHgap(5);
 
-        GridPane filesPane = new GridPane();
-        filesPane.setStyle("-fx-background-color: #bebeb6");
-        //TODO Fill gridpane with file icons with filename
-        for (int col = 0; col < files.length; col++) {
-            filesPane.add(new Text(files[col]), col, 0);
-        }
 
-        body.add(filesPane, 0, 0);
+        VBox projectInfoBox = new VBox();
+        projectInfoBox.getChildren().add(new Text("Project Generated Summary info"));
+        body.add(projectInfoBox, 0, 0);
 
-        VBox summaryBox = new VBox();
-        summaryBox.getChildren().add(new Text("Generated Summary info"));
-        body.add(summaryBox, 1, 0);
+        VBox filesBox = new VBox();
+        Button addFileButton = new Button("Add file", new FontIcon("mdi-plus-box"));
+        filesBox.getChildren().addAll(addFileButton, displayFiles());
+        body.add(filesBox, 1, 0);
 
-        ColumnConstraints fileCol = new ColumnConstraints();
-        fileCol.setPercentWidth(75);
-        ColumnConstraints summaryCol = new ColumnConstraints();
-        summaryCol.setPercentWidth(20);
-        body.getColumnConstraints().addAll(fileCol, summaryCol);
+        VBox fileInfoBox = new VBox();
+        fileInfoBox.getChildren().add(new Text("File Generated Summary Info"));
+        body.add(fileInfoBox, 2, 0);
+
+
+        ColumnConstraints projectSummaryCol = new ColumnConstraints();
+        projectSummaryCol.setPercentWidth(15);
+        ColumnConstraints filesCol = new ColumnConstraints();
+        filesCol.setPercentWidth(70);
+        ColumnConstraints fileSummaryCol = new ColumnConstraints();
+        fileSummaryCol.setPercentWidth(15);
+        body.getColumnConstraints().addAll(projectSummaryCol, filesCol, fileSummaryCol);
 
         RowConstraints bodyRow = new RowConstraints();
         bodyRow.setPercentHeight(900);
@@ -66,5 +67,40 @@ public class ProjectView extends VBox {
         this.getChildren().addAll(menuBar, body);
 
 
+    }
+
+
+    GridPane displayFiles() {
+
+        GridPane filesPane = new GridPane();
+        filesPane.setStyle("-fx-background-color: #bebeb6");
+        filesPane.setPadding(new Insets(10, 10, 10, 10));
+        filesPane.setHgap(10);
+        filesPane.setVgap(10);
+
+        for (int col = 0; col < files.length; col++) {
+
+            String extension = "";
+            int i = files[col].lastIndexOf('.');
+            if (i > 0) extension = files[col].substring(i+1);
+
+            FontIcon fileIcon = new FontIcon();
+            switch (extension) {
+                case "pdf":
+                    fileIcon.setIconLiteral("mdi-file-pdf");
+                    break;
+                case "xlsx":
+                    fileIcon.setIconLiteral("mdi-file-excel");
+                    break;
+                case "png", "jpg":
+                    fileIcon.setIconLiteral("mdi-file-image");
+                    break;
+            }
+
+            Button fileButton = new Button(files[col], fileIcon);
+            fileButton.setPrefHeight(30);
+            filesPane.add(fileButton, col, 0);
+        }
+        return filesPane;
     }
 }
