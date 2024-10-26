b8048525c72c945e46782cea6d189507b1d40f80
wwj033 <wwj033@usask.ca>
2024-10-26 13:06:24 -0600

    Created a project class

---
+++ src/main/java/com/cmpt370T7/PRJFlow/Project.java
@@ -0,0 +1,16 @@
+package com.cmpt370T7.PRJFlow;
+
+import java.util.ArrayList;
+
+public class Project {
+
+    String name;
+    ArrayList<String> fileNames;
+
+    Project(String name) {
+        this.name = name;
+        this.fileNames = new ArrayList<>();
+    }
+
+
+}
