560bd62bf762fcdad031e8364a2901ddba951b27
Ryan Schaffer (rys686) <rys686@mail.usask.ca>
2024-11-20 11:03:28 +0000

    Moved initialization logic to PRJFlow inside app execution, still no manual forcing of initialization, but helper will display on startup if no provider is selected.

--- src/main/java/com/cmpt370T7/PRJFlow/PRJFlow.java
+++ src/main/java/com/cmpt370T7/PRJFlow/PRJFlow.java
@@ -3,8 +3,9 @@ package com.cmpt370T7.PRJFlow;
 import java.io.IOException;
 
 import com.cmpt370T7.PRJFlow.gui.GUI;
-import com.cmpt370T7.PRJFlow.llm.ProviderHelper;
+import com.cmpt370T7.PRJFlow.llm.*;
 import com.cmpt370T7.PRJFlow.util.AlertHelper;
+import javafx.application.Platform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +20,13 @@ public class PRJFlow extends Application {
     public void start(Stage stage) {
         logger.info("Starting application...");
 
-        try {
-            AppDataManager.instantiate();
-            logger.debug("AppDataManager instantiated");
-        } catch (IOException e) {
-            logger.error("Failed to start appdata manager", e);
-            // TODO Display error to user.
-        }
+        initialize_appdata();
+        start_mainui(stage);
+        initialize_llms(stage);
+        setup_on_exit(stage);
+    }
 
+    private void start_mainui(Stage stage) {
         GUI root = new GUI();
         Scene scene = new Scene(root);
         stage.setTitle("PRJFlow");
@@ -34,10 +34,52 @@ public class PRJFlow extends Application {
         stage.setMaximized(true);
 
         stage.show();
+        root.requestFocus();
+    }
 
-        ProviderHelper helper = new ProviderHelper(stage);
-        helper.showProviderSelectionDialog();
+    private void initialize_appdata() {
+        try {
+            AppDataManager.instantiate();
+            logger.debug("AppDataManager instantiated");
+        } catch (IOException e) {
+            logger.error("Failed to start appdata manager", e);
+            AlertHelper.showError("Error", "Failed to start AppDataManager, please check logs for more information.");
+        }
+    }
 
+    private void initialize_llms(Stage stage) {
+        // Check if the user has set up the provider
+        ConfigManager.LlmProviderConfig providerConfig = AppDataManager.getInstance().getConfigManager().getLlmProviderConfig();
+        if (providerConfig == null) {
+            logger.info("No LLM provider set up, prompting user to set up provider...");
+            Platform.runLater(() -> {
+                ProviderHelper providerHelper = new ProviderHelper(stage);
+                providerHelper.showProviderSelectionDialog().thenRun(() -> {
+                    // this is sloppy. but it works for now.
+                    logger.info("First time setup complete, starting LLM provider...");
+                    ConfigManager.LlmProviderConfig afterSetupProviderConfig = AppDataManager.getInstance().getConfigManager().getLlmProviderConfig();
+                    instantiateAiEngine(afterSetupProviderConfig);
+                });
+            });
+        } else {
+            instantiateAiEngine(providerConfig);
+        }
+    }
+
+    private void instantiateAiEngine(ConfigManager.LlmProviderConfig providerConfig) {
+        logger.info("Starting connection with LLM provider: {}", providerConfig.provider());
+        switch (providerConfig.provider()) {
+            case "GroqCloud" -> AiEngine.instantiate(CloudLlmProvider.createGroqProvider(providerConfig.key()));
+            case "OpenAI" -> AiEngine.instantiate(CloudLlmProvider.createOpenAIProvider(providerConfig.key()));
+            case "Ollama" -> AiEngine.instantiate(new OllamaProvider());
+            default -> {
+                logger.error("Invalid provider name in config file: {}", providerConfig.provider());
+                AlertHelper.showError("Error", "Invalid provider name in config file, please check logs for more information.");
+            }
+        }
+    }
+
+    private void setup_on_exit(Stage stage) {
         stage.setOnCloseRequest(e -> {
             logger.info("Closing application...");
             try {
@@ -50,8 +92,6 @@ public class PRJFlow extends Application {
                 e.consume();
             }
         });
-
-        root.requestFocus();
     }
 
     public static void main(String[] args) {
